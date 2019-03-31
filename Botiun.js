c2da7974464625dde606a1d612556df1e8dab2e2
Greg Kilmer <remlik.mk@gmail.com>
2019-03-31 17:49:05 -0400

    Setup querying of users in chat every set interval and updating join, stay, and part handlers based on that. Also added currency and time in stream tracking to the database

--- Botiun.js
+++ Botiun.js
@@ -3,13 +3,27 @@
 const tmi = require( 'tmi.js' );
 const fs = require( 'fs' );
 const process = require( 'process' );
+const axios = require( 'axios' );
 const constants = require( './Constants.js' );
 const database = require( './Database.js' );
 
 const VERBOSE = true;
+const UPDATE_INTERVAL = 30; //Seconds
+const S_TO_MS = 1000;
+const CURRENCY_PER_INTERVAL = 1;
 const modules = [];
 const channel = "Patiun";
 const superUsers = [ 'patiun' ];
+var lastUsers = {
+  broadcaster: [],
+  vips: [],
+  moderators: [],
+  staff: [],
+  admins: [],
+  global_mods: [],
+  viewers: []
+};
+var currentUsers = [];
 var ignoredUsers = [];
 var live = false;
 const opts = {
@@ -22,8 +36,8 @@ var stdin = process.openStdin();
 
 client.on( 'connected', onConnectedHandler );
 client.on( 'message', onMessageHandler );
-client.on( 'join', onJoinHandler );
-client.on( 'part', onPartHandler );
+//client.on( 'join', onJoinHandler );
+//client.on( 'part', onPartHandler );
 
 client.connect();
 
@@ -33,25 +47,195 @@ client.connect();
 
 function onConnectedHandler( addr, port ) {
   ignoredUsers = constants.ignoredUsers;
+  currentUsers = [];
   initializeAllModules();
   log( `Connected to Twitch on ${addr}:${port}` );
+  //DEBUG-------------------------!!!!!!!!!!!!!!!!!!
+  checkForViewerChanges();
+  setInterval( checkForViewerChanges, UPDATE_INTERVAL * S_TO_MS );
 }
 
-function onJoinHandler( target, username, self ) {
-  /*
-  if ( ignoredUsers.includes( username ) || username === 'patiun' ) {
-    return;
-  }
-  */
-  log( `${username} has joined the channel` );
-  database.get( constants.collectionUsers, {
-    twitchID: username
+
+function checkForViewerChanges() {
+  axios.get( constants.chatterEndpoint ).then( response => {
+    console.log( "-------------------------------------------" );
+    let tmpUsers = response.data.chatters;
+    gatherDifferences( lastUsers, tmpUsers ).then( ( differences ) => {
+      let keys = Object.keys( differences.stay );
+      for ( let i = 0; i < keys.length; i++ ) {
+        let key = keys[ i ];
+        for ( let j = 0; j < differences.join[ key ].length; j++ ) {
+          joinUser( differences.join[ key ][ j ] );
+        }
+        for ( let j = 0; j < differences.stay[ key ].length; j++ ) {
+          stayUser( differences.stay[ key ][ j ] );
+        }
+        for ( let j = 0; j < differences.part[ key ].length; j++ ) {
+          partUser( differences.part[ key ][ j ] );
+        }
+      }
+    } );
+    lastUsers = tmpUsers;
+
+  } ).catch( error => {
+    console.log( error );
   } );
 }
 
-function onPartHandler( target, username, self ) {
-  log( `${username} has left the channel` );
+function stayUser( username ) {
+  if ( ignoredUsers.includes( username ) ) {
+    return;
+  }
+
+  log( `${username} is still in the channel` );
+
+  database.get( constants.collectionUsers, {
+    twitchID: username
+  } ).then( ( result ) => {
+
+    if ( result.length > 0 ) {
+      let tmpUser = result[ 0 ];
+      if ( tmpUser.currency === undefined ) {
+        tmpUser.currency = 0;
+      }
+      if ( tmpUser.timeInStream === undefined ) {
+        tmpUser.timeInStream = 0;
+      }
+      let updatedUser = {
+        $set: {
+          currency: tmpUser.currency + CURRENCY_PER_INTERVAL,
+          timeInStream: tmpUser.timeInStream + UPDATE_INTERVAL
+        }
+      }
+      database.update( constants.collectionUsers, {
+        twitchID: username
+      }, updatedUser );
+    }
+  } ).catch( () => {
+    console.log( "[ERROR]: (Botiun.js onJoinHandler GET) Something went wrong! " );
+  } );
 }
+
+function gatherDifferences( oldUsers, newUsers ) {
+  return new Promise( function ( resolve, reject ) {
+    let stay = {};
+    let join = {};
+    let part = {};
+    //Old and new must have the same keys
+    let keys = Object.keys( newUsers );
+    for ( let i = 0; i < keys.length; i++ ) {
+      stay[ keys[ i ] ] = [];
+      join[ keys[ i ] ] = [];
+      part[ keys[ i ] ] = [];
+      let oldArr = oldUsers[ keys[ i ] ];
+      let newArr = newUsers[ keys[ i ] ];
+      if ( oldArr != undefined ) {
+        for ( let j = 0; j < oldArr.length; j++ ) {
+          let oldUser = oldArr[ j ];
+          if ( newArr.includes( oldUser ) ) {
+            stay[ keys[ i ] ].push( oldUser );
+          } else {
+            part[ keys[ i ] ].push( oldUser );
+          }
+        }
+        for ( let j = 0; j < newArr.length; j++ ) {
+          let newUser = newArr[ j ];
+          if ( !oldArr.includes( newUser ) ) {
+            join[ keys[ i ] ].push( newUser );
+          }
+        }
+      }
+    }
+    var diff = {
+      stay: stay,
+      join: join,
+      part: part
+    };
+    resolve( JSON.parse( JSON.stringify( diff ) ) );
+  } );
+}
+
+//////////////////
+///// JOIN //////
+////////////////
+function onJoinHandler( target, username, self ) {
+  JoinUser( username );
+}
+
+function joinUser( username ) {
+  if ( ignoredUsers.includes( username ) ) {
+    return;
+  }
+
+  log( `${username} has joined the channel` );
+  if ( !currentUsers.includes( username ) ) {
+    currentUsers.push( username );
+  }
+
+
+  database.get( constants.collectionUsers, {
+    twitchID: username
+  } ).then( ( result ) => {
+
+    let d = new Date();
+    let dateString = d.getUTCDate().toString();
+    if ( result.length > 0 ) {
+      //Update the last time the user joined
+      database.update( constants.collectionUsers, {
+        twitchID: username
+      }, {
+        $set: {
+          lastJoin: d
+        }
+      } );
+    } else {
+      //Update the last time the user joined
+      let newUser = getNewUserTemplate();
+      newUser.twitchID = username;
+      newUser.lastJoin = d;
+      database.insert( constants.collectionUsers, newUser );
+    }
+  } ).catch( () => {
+    console.log( "[ERROR]: (Botiun.js onJoinHandler GET) Something went wrong! " );
+  } );
+}
+
+//////////////////
+///// PART //////
+////////////////
+function onPartHandler( target, username, self ) {
+  partUser( username );
+}
+
+function partUser( username ) {
+  if ( ignoredUsers.includes( username ) ) {
+    return;
+  }
+
+  if ( currentUsers.includes( username ) ) {
+    var updated = currentUsers.filter( function ( value, index, arr ) {
+      return value != username;
+    } );
+    currentUsers = updated;
+  }
+
+  log( `${username} has left the channel` );
+
+  //Update the last time we saw the user
+  let d = new Date();
+  let dateString = d.getUTCDate().toString();
+  database.update( constants.collectionUsers, {
+    twitchID: username
+  }, {
+    $set: {
+      lastPart: d
+    }
+  } );
+}
+
+//////////////////
+//// Message ////
+////////////////
 
 function onMessageHandler( target, context, msg, self ) {
   if ( self ) {
@@ -59,6 +243,27 @@ function onMessageHandler( target, context, msg, self ) {
   }
   var username = context[ 'username' ];
   log( `Incoming message from ${username}: "${msg}"` );
+
+  //Add one to the message count
+  database.get( constants.collectionUsers, {
+    twitchID: username
+  } ).then( ( result ) => {
+    for ( let i = 0; i < result.length; i++ ) {
+      let count = result[ i ].messages + 1;
+      if ( count === undefined || isNaN( count ) ) {
+        count = 1;
+      }
+      database.update( constants.collectionUsers, {
+        twitchID: username
+      }, {
+        $set: {
+          messages: count
+        }
+      } );
+    }
+  } ).catch( () => {
+    console.log( "[ERROR]: (Botiun.js onMessageHandler GET) Something went wrong! " );
+  } );
 
   var msgStripped = msg.trim();
   var msgTokens = msgStripped.split( ' ' );
@@ -82,6 +287,9 @@ function onMessageHandler( target, context, msg, self ) {
 function startStream() {
   log( "Starting Stream..." );
   live = true;
+  let nu = getNewUserTemplate();
+  nu.messages = 10;
+  console.log( nu );
 }
 
 function endStream() {
@@ -97,6 +305,23 @@ function initializeAllModules() {
     log( `${modules[i].name} initialized` );
   }
   log( 'All modules intitialized' );
+}
+
+function getNewUserTemplate() {
+  var userTemplate = {
+    twitchID: "NONE",
+    messages: 0,
+    lastJoin: null,
+    lastPart: null,
+    timeInStream: 0,
+    currency: 0,
+    isSub: false,
+    isVIP: false,
+    isMod: false,
+    isHappyPerson: false
+  };
+
+  return JSON.parse( JSON.stringify( userTemplate ) );
 }
 
 function handleCommands( target, context, self, msgTokens ) {
@@ -141,6 +366,11 @@ stdin.addListener( "data", function ( d ) {
 
   if ( [ 'start' ].includes( msgTokens[ 0 ].toLowerCase() ) ) {
     startStream();
+  }
+
+  if ( [ 'who' ].includes( msgTokens[ 0 ].toLowerCase() ) ) {
+    log( "Current Users:" );
+    log( currentUsers );
   }
 
   if ( [ 'say' ].includes( msgTokens[ 0 ].toLowerCase() ) ) {
--- Database.js
+++ Database.js
@@ -4,63 +4,51 @@ const constants = require( './Constants.js' );
 var MongoClient = require( 'mongodb' ).MongoClient;
 var url = constants.databaseAddress;
 
-async function getAsync( collection, query, projection ) {
-  var data = [];
-  let promise = new Promise( ( resolve, reject ) => {
-    MongoClient.connect( url, function ( err, db ) {
-      if ( err ) reject();
+function get( collection, query, projection ) {
+  return new Promise( function ( resolve, reject ) {
+    MongoClient.connect( url, {
+      useNewUrlParser: true
+    }, function ( err, db ) {
       var dbo = db.db( constants.databaseName );
       dbo.collection( collection ).find( query, projection ).toArray( function ( err, result ) {
-        if ( err ) reject();
-        console.log( result );
-        data = result;
-        db.close();
         resolve( result );
+        db.close();
       } );
-    } );
-  } );
-  await promise();
-  return data;
-}
-
-function get( collection, query, projection ) {
-  MongoClient.connect( url, function ( err, db ) {
-    if ( err ) throw err;
-    var dbo = db.db( constants.databaseName );
-    dbo.collection( collection ).find( query, projection ).toArray( function ( err, result ) {
-      if ( err ) throw err;
-      console.log( result );
-      db.close();
     } );
   } );
 }
 
 function insert( collection, newData ) {
-  MongoClient.connect( url, function ( err, db ) {
-    if ( err ) throw err;
-    var dbo = db.db( constants.databaseName );
-    dbo.collection( collection ).insertOne( newData, function ( err, res ) {
+  MongoClient.connect( url, {
+      useNewUrlParser: true
+    },
+    function ( err, db ) {
       if ( err ) throw err;
-      console.log( "1 document inserted" );
-      db.close();
+      var dbo = db.db( constants.databaseName );
+      dbo.collection( collection ).insertOne( newData, function ( err, res ) {
+        if ( err ) throw err;
+        console.log( "1 document inserted" );
+        db.close();
+      } );
     } );
-  } );
 }
 
 function update( collection, query, newData ) {
-  MongoClient.connect( url, function ( err, db ) {
-    if ( err ) throw err;
-    var dbo = db.db( constants.databaseName );
-    dbo.collection( collection ).updateOne( query, newData, function ( err, res ) {
+  MongoClient.connect( url, {
+      useNewUrlParser: true
+    },
+    function ( err, db ) {
       if ( err ) throw err;
-      console.log( "1 document updated" );
-      db.close();
+      var dbo = db.db( constants.databaseName );
+      dbo.collection( collection ).updateOne( query, newData, function ( err, res ) {
+        if ( err ) throw err;
+        console.log( "1 document updated" );
+        db.close();
+      } );
     } );
-  } );
 }
 
 module.exports = {
-  getAsync: getAsync,
   get: get,
   insert: insert,
   update: update
